50d5e0ed7bcb388c0a3543e7f788bd51a60002f6
mhrndiva <ariadiva00@gmail.com>
2023-10-04 16:09:36 +0700

    pertemuan 2

---
+++ Pertemuan 2/Praktikum/p2_1_71420050/p2_1_71420050/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2_1_71420050
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Masukkan angka pertama");
+            float Angka1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan angka pertama");
+            float Angka2 = Convert.ToInt32(Console.ReadLine());
+
+            float penjumlahan;
+            float pengurangan;
+            float perkalian;
+            float pembagian;
+
+            penjumlahan = Angka1 + Angka2;
+            Console.WriteLine("{0} + {1} = {2}", Angka1, Angka2, penjumlahan);
+            pengurangan = Angka1 - Angka2;
+            Console.WriteLine("{0} - {1} = {2}", Angka1, Angka2, pengurangan);
+            perkalian = Angka1 * Angka2;
+            Console.WriteLine("{0} * {1} = {2}", Angka1, Angka2, perkalian);
+            pembagian = Angka1 / Angka2;
+            Console.WriteLine("{0} / {1} = {2}", Angka1, Angka2, pembagian);
+
+
+        }
+    }
+}
