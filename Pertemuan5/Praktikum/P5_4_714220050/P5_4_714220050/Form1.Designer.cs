5a256e48b523d63e12bd0c40f58dbdb5f0a8f559
mhrndiva <ariadiva00@gmail.com>
2023-10-31 16:26:32 +0700

    pertemuan5

---
+++ Pertemuan5/Praktikum/P5_4_714220050/P5_4_714220050/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace P5_4_714220050
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textNama = new System.Windows.Forms.TextBox();
+            this.CB_Kelamin = new System.Windows.Forms.ComboBox();
+            this.waktu = new System.Windows.Forms.DateTimePicker();
+            this.kelas = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.jadwal = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.kelas.SuspendLayout();
+            this.jadwal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(244, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(244, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Jenis Kelamin";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(244, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tanggal Lahir";
+            // 
+            // textNama
+            // 
+            this.textNama.Location = new System.Drawing.Point(352, 66);
+            this.textNama.Name = "textNama";
+            this.textNama.Size = new System.Drawing.Size(100, 20);
+            this.textNama.TabIndex = 3;
+            this.textNama.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // CB_Kelamin
+            // 
+            this.CB_Kelamin.FormattingEnabled = true;
+            this.CB_Kelamin.Items.AddRange(new object[] {
+            "Laki-Laki",
+            "Perempuan"});
+            this.CB_Kelamin.Location = new System.Drawing.Point(352, 105);
+            this.CB_Kelamin.Name = "CB_Kelamin";
+            this.CB_Kelamin.Size = new System.Drawing.Size(121, 21);
+            this.CB_Kelamin.TabIndex = 4;
+            this.CB_Kelamin.Text = "--PIlih Jenis Kelamin--";
+            this.CB_Kelamin.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // waktu
+            // 
+            this.waktu.Location = new System.Drawing.Point(352, 147);
+            this.waktu.Name = "waktu";
+            this.waktu.Size = new System.Drawing.Size(200, 20);
+            this.waktu.TabIndex = 5;
+            this.waktu.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // kelas
+            // 
+            this.kelas.Controls.Add(this.checkBox1);
+            this.kelas.Controls.Add(this.checkBox2);
+            this.kelas.Controls.Add(this.checkBox8);
+            this.kelas.Controls.Add(this.checkBox3);
+            this.kelas.Controls.Add(this.checkBox7);
+            this.kelas.Controls.Add(this.checkBox4);
+            this.kelas.Controls.Add(this.checkBox6);
+            this.kelas.Controls.Add(this.checkBox5);
+            this.kelas.Location = new System.Drawing.Point(145, 222);
+            this.kelas.Name = "kelas";
+            this.kelas.Size = new System.Drawing.Size(200, 144);
+            this.kelas.TabIndex = 6;
+            this.kelas.TabStop = false;
+            this.kelas.Text = "Pilih Kelas";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 29);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(49, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Biola";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 56);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(48, 17);
+            this.checkBox2.TabIndex = 13;
+            this.checkBox2.Text = "Gitar";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(114, 107);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(73, 17);
+            this.checkBox8.TabIndex = 19;
+            this.checkBox8.Text = "Komposer";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 84);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 14;
+            this.checkBox3.Text = "Saxophone";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(114, 84);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(53, 17);
+            this.checkBox7.TabIndex = 18;
+            this.checkBox7.Text = "Vokal";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(6, 107);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(75, 17);
+            this.checkBox4.TabIndex = 15;
+            this.checkBox4.Text = "Konduktor";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(114, 52);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(51, 17);
+            this.checkBox6.TabIndex = 17;
+            this.checkBox6.Text = "Drum";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(114, 29);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(53, 17);
+            this.checkBox5.TabIndex = 16;
+            this.checkBox5.Text = "Piano";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // jadwal
+            // 
+            this.jadwal.Controls.Add(this.radioButton4);
+            this.jadwal.Controls.Add(this.radioButton3);
+            this.jadwal.Controls.Add(this.radioButton2);
+            this.jadwal.Controls.Add(this.radioButton1);
+            this.jadwal.Location = new System.Drawing.Point(402, 222);
+            this.jadwal.Name = "jadwal";
+            this.jadwal.Size = new System.Drawing.Size(200, 144);
+            this.jadwal.TabIndex = 7;
+            this.jadwal.TabStop = false;
+            this.jadwal.Text = "Pilih_Jadwal";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 110);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(129, 17);
+            this.radioButton4.TabIndex = 11;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Minggu, 13.00 - 17.00";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 83);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(169, 17);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Sabtu && Minggu, 09.00 - 11.00";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 55);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(166, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Selasa && Kamis, 14.00 - 16.00";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(159, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Senin && Rabu, 14.00 - 16.00";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Tampilkan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(365, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Selesai";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(308, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "FORM PENDAFTARAN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.jadwal);
+            this.Controls.Add(this.kelas);
+            this.Controls.Add(this.waktu);
+            this.Controls.Add(this.CB_Kelamin);
+            this.Controls.Add(this.textNama);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.kelas.ResumeLayout(false);
+            this.kelas.PerformLayout();
+            this.jadwal.ResumeLayout(false);
+            this.jadwal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textNama;
+        private System.Windows.Forms.ComboBox CB_Kelamin;
+        private System.Windows.Forms.DateTimePicker waktu;
+        private System.Windows.Forms.GroupBox kelas;
+        private System.Windows.Forms.GroupBox jadwal;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Pertemuan5/Praktikum/P5_4_714220050/P5_4_714220050/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_4_714220050
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string label2 = CB_Kelamin.SelectedItem.ToString();
+            DateTime label3 = waktu.Value;
+
+
+            string kelas = "";
+            string jadwal = "";
+
+            if (checkBox1.Checked)
+            {
+                kelas += "Biola ";
+            }
+            if (checkBox2.Checked)
+            {
+                kelas += "Gitar ";
+            }
+            if (checkBox3.Checked)
+            {
+                kelas += "Saxophone ";
+            }
+            if (checkBox4.Checked)
+            {
+                kelas += "Konduktor ";
+            }
+            if (checkBox5.Checked)
+            {
+                kelas += "Piano ";
+            }
+            if (checkBox6.Checked)
+            {
+                kelas += "Drum ";
+            }
+            if (checkBox7.Checked)
+            {
+                kelas += "Vokal ";
+            }
+            if (checkBox8.Checked)
+            {
+                kelas += "Komposer ";
+            }
+            else if (string.IsNullOrEmpty(kelas))
+            {
+                MessageBox.Show("Anda haru memilih kelas", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (radioButton1.Checked)
+            {
+                jadwal = "Senin & Rabu, 14.00 - 16.0";
+            }
+            else if (radioButton2.Checked)
+            {
+                jadwal = "Selasa & Kamis, 14.00 - 16.00";
+            }
+            else if (radioButton3.Checked)
+            {
+                jadwal = "Minggu, 13.00 - 17.00";
+            }
+
+
+            MessageBox.Show(
+                "Nama: " + textNama.Text +
+                "\nJenis Kelamin: " + label2 +
+                "\nTanggal Lahir:" + label3.ToString("dd MMMM yyyy") +
+                "\nPilihan Kelas: " + kelas +
+                "\nPilihan Jadwal: " + jadwal, 
+                "Informasi Pendataran",
+                MessageBoxButtons.OK, MessageBoxIcon.Information) ;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
